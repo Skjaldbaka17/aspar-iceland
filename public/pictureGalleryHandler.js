a030ad377ec542d77016c3538387e519c1ef1ae6
Skjaldbaka17 <Skjaldbaka17@gmail.com>
2018-03-26 23:06:06 +0000

    all

--- public/pictureGalleryHandler.js
+++ public/pictureGalleryHandler.js
@@ -1,69 +1,68 @@
-function ImageGallery(){
-                this.PICTURES = document.getElementsByClassName('yoda');
-                var elem = document.getElementById("largeImageContainer");
-                elem.addEventListener("click", stopEvent, false);
-                this.current = 1;
-          }
+function ImageGallery() {
+    this.PICTURES = document.getElementsByClassName('yoda');
+    var elem = document.getElementById("largeImageContainer");
+    elem.addEventListener("click", stopEvent, false);
+    this.current = 1;
+}
 
-            ImageGallery.prototype.changePic = function (x){
+ImageGallery.prototype.changePic = function (x) {
 
-                this.current = (this.current+x)%this.PICTURES.length;
-                if(this.current < 0) this.current = this.PICTURES.length-1;
-
-
-
-                document.getElementById("largeImage").setAttribute('src', this.PICTURES[this.current].getAttribute('src'));
-            }
+    this.current = (this.current + x) % this.PICTURES.length;
+    if (this.current < 0) this.current = this.PICTURES.length - 1;
 
 
-          ImageGallery.prototype.closeModal = function(){
-                document.getElementById('myModal').style.display = "none";
-          }
 
-          ImageGallery.prototype.openModal = function(source) {
-            
-                var theModal = document.getElementById('myModal');
-                theModal.style.display = "block";
-                
-                var firstPic = source.replace('http://localhost:5000', '.');
-            
+    document.getElementById("largeImage").setAttribute('src', this.PICTURES[this.current].getAttribute('src'));
+}
 
 
-                var i;
-                for(i = 0; i < this.PICTURES.length; i++){
-                    if(this.PICTURES[i].getAttribute('src') == firstPic){ break;}      
-                }
-                alert(i + " " + firstPic);
+ImageGallery.prototype.closeModal = function () {
+    document.getElementById('myModal').style.display = "none";
+}
 
-                this.current = i;
+ImageGallery.prototype.openModal = function (source) {
 
-                document.getElementById("largeImage").setAttribute('src', this.PICTURES[this.current].getAttribute('src'));
-                alert(this.PICTURES[this.current].getAttribute('src'));
-            }
+    const theModal = document.getElementById('myModal');
+    theModal.style.display = 'block';
 
-            var imageGal = new ImageGallery();
+    const firstPic = source.replace('http://localhost:5000', '.');
 
 
-           function stopEvent (ev) {
-                // this ought to keep myModal from getting the click.
-                ev.stopPropagation();
-                }
-    
-            $('document').ready(function() {
-                
-                size_li = $("#myList li").length;
-                 
-                if(size_li <=9 ) $('#loadMore').hide();
-                else $('#loadMore').show();
-                 x=9;
-                 $('#myList li:lt('+x+')').show();
-                 $('#loadMore').click(function (e) {
-                    e.preventDefault();
-                 x= (x+9 <= size_li) ? x+9 : size_li;
-                 if(x >= size_li) $('#loadMore').hide();
-                 $('#myList li:lt('+x+')').show();
-                 });
-            });
+
+    var i;
+    for (i = 0; i < this.PICTURES.length; i++) {
+        if (this.PICTURES[i].getAttribute('src') === firstPic) { break; }
+    }
+    alert(i + " " + firstPic);
+
+    this.current = i;
+
+    document.getElementById("largeImage").setAttribute('src', this.PICTURES[this.current].getAttribute('src'));
+    alert(this.PICTURES[this.current].getAttribute('src'));
+}
+
+const imageGal = new ImageGallery();
 
 
-                
+function stopEvent(ev) {
+    // this ought to keep myModal from getting the click.
+    ev.stopPropagation();
+}
+
+$('document').ready(function () {
+
+    size_li = $("#myList li").length;
+
+    if (size_li <= 9) $('#loadMore').hide();
+    else $('#loadMore').show();
+    x = 9;
+    $('#myList li:lt(' + x + ')').show();
+    $('#loadMore').click(function (e) {
+        e.preventDefault();
+        x = (x + 9 <= size_li) ? x + 9 : size_li;
+        if (x >= size_li) $('#loadMore').hide();
+        $('#myList li:lt(' + x + ')').show();
+    });
+});
+
+
